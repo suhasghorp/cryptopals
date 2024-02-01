362e1319bd4289262064af436201e8819b9b8e44
Suhas Ghorpadkar <suhasghorp@gmail.com>
2024-02-01 13:34:47 -0500

    fixed

--- src/challenge8.rs
+++ src/challenge8.rs
@@ -1,19 +1,24 @@
-const BLOCK_SIZE:usize = 16;
+use std::collections::HashSet;
 
-pub fn bytes_to_chunks(bytes:&[u8], chunk_size:usize) -> Vec<Vec<&[u8]>>{
+pub fn bytes_to_chunks(bytes:&[u8], chunk_size:usize) -> HashSet<&[u8]> {
+    
     (0..bytes.len())
     .step_by(chunk_size)
-    .map(|x|&bytes[x..x + chunk_size])
-    .collect()
+    .map(|ind| &bytes[ind..ind + chunk_size])
+    .collect::<Vec<_>>()
+    .into_iter()
+    .collect::<HashSet<&[u8]>>()
+    
 }
 
 #[cfg(test)]
 mod set1_eight_tests{
     use super::*;  
-    use std::path::Path;
     use std::fs::File;
     use std::io::{BufRead, BufReader};
     use crate::challenge1;
+    const BLOCK_SIZE:usize = 16;
+
     #[test]
     fn aes_ecb_dec_test(){
         let path = "/home/sghorp/rustprojects/cryptopals/data/challenge8.txt";
@@ -25,7 +30,15 @@ mod set1_eight_tests{
         }
 
         for (idx, ciphertext) in ciphertexts.iter().enumerate(){
-            let num_block = ciphertext.len() / BLOCK_SIZE;
+            let num_blocks = ciphertext.len() / BLOCK_SIZE;
+            let unique_chunks = bytes_to_chunks(&ciphertext, BLOCK_SIZE);
+            let num_unique_blocks = unique_chunks.len();
+            let repeated_blocks = num_blocks - num_unique_blocks;
+            if repeated_blocks == 0{
+                continue;
+            }
+            println!("Line {} has {} repeated blocks and is likely using ECB", idx, repeated_blocks);
         }
+        assert_eq!(1,1);
     }
 }
